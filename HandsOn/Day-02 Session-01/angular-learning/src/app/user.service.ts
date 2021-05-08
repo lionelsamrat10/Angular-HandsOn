ffc6b2449cc0feaaa6b96a3f063ad5e91622c98b
SAMRAT MITRA <samratmitra338@gmail.com>
2021-05-09 01:21:03 +0530

    Update user.service.ts

--- HandsOn/Day-02 Session-01/angular-learning/src/app/user.service.ts
+++ HandsOn/Day-02 Session-01/angular-learning/src/app/user.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
+import { catchError, retry, map, tap, filter } from 'rxjs/operators';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
@@ -9,4 +10,26 @@ import { catchError, retry } from 'rxjs/operators';
 export class UserService {
 
   constructor(private http: HttpClient) { }
+  //Create the GET request
+  getAllUsers() { 
+    return this.http.get<User[]>("https://reqres.in/api/users?page=2");
+  }
+  //Create the POST request
+  addUser(user: User){
+    return this.http.post<User>("https://reqres.in/api/users?page=2",user);
+  }
+  //Create the PUT request
+  updateUser(user: User) {
+    return this.http.put<User>("https://reqres.in/api/users?page=2&id=${user.id}",user);
+  }
+  //Create the Delete Request
+  deleteUser(id: number) {
+    return this.http.delete("https://reqres.in/api/users?page=2&id=${id}");
+  }
+  //Create a register method
+  register(user: User){
+    return this.http.post("https://reqres.in/api/register", user).pipe(catchError((error:HttpErrorResponse) => {
+      console.log(error.error);
+      return throwError("Error checking successful");}));
+  }
 }
